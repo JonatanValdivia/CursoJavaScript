3d4fa1dab91946e0c2cd3940b83b08e8942685c0
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:32:18 -0300

    Constantes

---
+++ 03.js
@@ -0,0 +1,12 @@
+//Constantes -> não há como alterar/atribuir (algo) seu valor;
+//Possível:
+var canal = "CFB Cursos";
+canal += " - JavaScript<br>";
+document.write(canal);
+
+//Não posssível:
+/*const canal = "CFB Cursos";
+canal += " - JavaScript";
+document.write(canal);*/
+                  //Constante pré-definida 
+document.write(Math.PI);
