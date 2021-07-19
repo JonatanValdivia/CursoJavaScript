631269435610b89d79a0ce478e1ebf3ac41c7972
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 11:50:27 -0300

    InnerHTML e mais sobre getElementById()

---
+++ 24.js
@@ -0,0 +1,6 @@
+//innerHTML e mais sobre getElementById
+const caixa = document.getElementById('box');
+caixa.innerHTML = "CURSOS JAVASCRIPT";
+
+const input = document.getElementById('input');
+input.value = 'CFB Curso de JavaScript' 
