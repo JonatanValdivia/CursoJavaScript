b9753c07f76b3dafea20ad8856c0bb0edf502f1c
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:36:33 -0300

    Comfirm

---
+++ 07.js
@@ -0,0 +1,16 @@
+//If
+var v1, v2;
+v1 = 32;
+v2 = 9;
+
+if(v1 > v2){
+  //True
+  document.write('Verdadeiro');
+}else{
+  //False
+  document.write("Falso");
+}
+
+if(1){ //Usar o 0 para false
+  document.write("<br><br>Retorno: true")
+}
