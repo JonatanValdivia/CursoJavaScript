37b2438c5e06c6ac3888de23bc4ba17292e4f05a
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:50:28 -0300

    Incremento e decremento

---
+++ 17.js
@@ -0,0 +1,20 @@
+//Incremento e decremento de variáveis. -> formas de incrementar ou decrementar variáveis.
+
+//pré-incremento e pós-decremento:
+
+//Pré-incremento
+var num = 10;
+//num = num+1;
+//Ainda assim, exibirá dez
+console.log(num++);
+//Aqui será dez:
+console.log(num);  
+//Exibirá doze (por conta do pós-decremento acima):
+console.log(++num)
+
+
+//Passar números para negativo ou positivo:
+var numero = 8;
+//numero=numero*-1
+numero*=-1;
+console.log(numero)
