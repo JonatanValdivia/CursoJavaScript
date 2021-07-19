24acfc560036aa8741c091b83ec9848b90760e16
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:54:03 -0300

    Do While

---
+++ 20.js
@@ -0,0 +1,9 @@
+//Do while -> retorna algo (valor de determinada variável) mesmo que tal teste lógico seja false
+var num = 1;
+
+do{
+  document.write(num + '<br>')  
+  num++;
+}while(num <= 10)
+
+
