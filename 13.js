96bc61063c0b5428a5f0e7b930eaa22d8b2a0a5e
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:44:32 -0300

    Array - unidimencional

---
+++ 13.js
@@ -0,0 +1,23 @@
+//Array - unidimencional - vetor - controlado por um único índice
+
+//var cor = new Array();
+var cor = [];
+/*
+cor.push("Branco"); //[0]
+cor.push("Vermelho"); //[1]
+cor.push("Azul"); //[2]
+cor.push("Cinza"); //[3]
+cor.push("Verde"); //[4]
+*/
+
+cor[0] = "Branco";
+cor[1] = "Preto";
+cor[2] = "Vermelho";
+cor[3] = "Azul";
+
+
+document.write(); 
+ d = new Date(ano, mes, dia, hora, minuto, segundo, milisegundos);
+ dia = d.
+document.write(d)
+
