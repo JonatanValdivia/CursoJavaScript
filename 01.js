f198e8e5856d8c519fbf1ad5d57185697ae152d5
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:25:25 -0300

    document.write

---
+++ 01.js
@@ -0,0 +1,13 @@
+//document.write('introdução');
+//Write faz uma impressão na tela, e não funciona sozinho, deve-se chamr o document primeiro
+//Também pode-se passar tags html dentro do write()
+//Também pode-se passar números inteiros ou não dentro dele dentro dele
+document.write("<b>Escreva na tela</b><br>");
+//Cncatena quando há uma String e um number
+document.write("50 " + 100 + "<br>");
+//Soma:
+document.write(100 + 40);
+//CSS inline também funciona
+document.write("<br><p style = 'color: red;'>Texto estilizado no css inline</p>")
+
+//Quando que se usa o write?
