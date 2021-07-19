7cb78202526425c29bc23740bf8deb8106a74c18
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 11:30:49 -0300

    getElementById()

---
+++ 23.js
@@ -0,0 +1,8 @@
+//getElementById -> pegar elementos através do ID.
+const caixa = document.getElementById('box');
+caixa.style.backgroundColor = 'red';
+caixa.style.fontSize = "50px";
+caixa.style.border = "2px solid black";
+caixa.style.borderRadius = "10px";
+caixa.style.padding = "2%";
+
