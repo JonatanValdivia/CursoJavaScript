3888786b6ccbb992fc68cda93a2b81304486cad6
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:30:17 -0300

    Escopo de variáveis

---
+++ 02.js
@@ -0,0 +1,19 @@
+//Variáveis
+//Criação: variável de escopo global
+/*var canal;
+var nome;
+var site;*/
+
+var canal = null, nome = "Jonatan", site;
+var numero = 333;
+document.write(` ${typeof numero} ${typeof nome} ${typeof site} <br>`);
+
+//declaração implicita:
+curso ="JavaScript"; // -> não recomendado!
+
+document.write(curso);
+
+var ano = 2017;
+
+document.write("<br>" + nome + " " + Number(numero + ano));
+
