fbb8259ee68dc4e5211bc15e2cbd6b57d645aad5
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-19 09:33:31 -0300

    Alert

---
+++ 04.js
@@ -0,0 +1,9 @@
+//Alert
+//Diferente do write, que é um método ca classe document, alert é um método do objeto window. O window não é explicito, ou seja, já estamos trabalhando nele, então não é necessário a chamada á ela.
+
+//alert('Utilizando o window.alert()');
+
+var nome = "Jonatan V. ";
+var sobrenome = "Valdivia";
+const num = 900;
+alert(nome + sobrenome + " " + Number(num + 9));
