abd7d1f4196c1911aee17cf336f2b58f187bdcd8
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-07-26 18:33:13 -0300

    Formataçao condicional - operando dados em tabelas

--- 40.js
+++ 40.js
@@ -10,10 +10,10 @@ for(var j = 1; j < tabela.rows.length; j++){
 }
 
 var notas = [];
-notas[0]= document.getElementById('n0')
-notas[1]= document.getElementById('n1')
-notas[2]= document.getElementById('n2')
-notas[3]= document.getElementById('n3')
+notas[0]= document.getElementById('n0');
+notas[1]= document.getElementById('n1');
+notas[2]= document.getElementById('n2');
+notas[3]= document.getElementById('n3');
 for(var i = 0; i <= 3; i++){
   if(notas[i].innerHTML < 50){
     notas[i].style.color = 'red';
